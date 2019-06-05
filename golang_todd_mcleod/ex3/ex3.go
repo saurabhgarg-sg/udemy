c0ca2e3e592c99f2bf2d0d6bff507e73f34c1ed0
Saurabh Garg <20387189+saurabhgarg-sg@users.noreply.github.com>
2019-06-05 14:54:55 -0700

    Exercise 3

---
+++ golang_todd_mcleod/ex3/ex3.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+var x = 42
+var y = "James Bond"
+var z = true
+
+func main() {
+	s := fmt.Sprintf("%d %s %t", x, y, z)
+	fmt.Println(s)
+	s = fmt.Sprintf("%v %v %v", x, y, z)
+	fmt.Println(s)
+}
